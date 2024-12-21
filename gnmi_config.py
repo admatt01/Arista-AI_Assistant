ec083db3e0da6a4a3c8d78e56f9d12a8642a7943
admatt01 <admatt01@gmail.com>
2024-12-22 09:39:24 +1100

    remove unused imports

--- gnmi_config.py
+++ gnmi_config.py
@@ -4,7 +4,6 @@ import json
 import yaml
 import asyncio
 import threading
-import dictdiffer
 from typing import List, Dict, Any, Union
 from pygnmi.client import gNMIclient
 from datetime import datetime, timezone
